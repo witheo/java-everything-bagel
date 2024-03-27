d04092d6d19615b55d7e99b974e716e38ebd7867
witheo <theosw@gmail.com>
2024-03-26 22:13:36 -0500

    Inital scafolding and notes on postgres instalation

---
+++ src/main/java/com/witheo/java_everything_bagel/JavaEverythingBagelApplication.java
@@ -0,0 +1,13 @@
+package com.witheo.java_everything_bagel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEverythingBagelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEverythingBagelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/witheo/java_everything_bagel/JavaEverythingBagelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.witheo.java_everything_bagel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEverythingBagelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
