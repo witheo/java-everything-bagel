640c72e61d039f89fc91868e89a733b202ffc288
witheo <theosw@gmail.com>
2024-04-04 20:18:03 -0500

    creating user

---
+++ src/main/java/com/witheo/java_everything_bagel/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.witheo.java_everything_bagel.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ src/test/java/com/witheo/java_everything_bagel/user/UserTest.java
@@ -0,0 +1,10 @@
+package com.witheo.java_everything_bagel.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+
+}
